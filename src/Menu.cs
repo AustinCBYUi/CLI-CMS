124c850b4d889eb9fede64360abee0adf92d2fb5
Austin Campbell <131830841+AustinCBYUi@users.noreply.github.com>
2025-01-15 21:04:52 -0700

    Working on menu class.

---
+++ src/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLI_CMS.src
+{
+    internal class Menu
+    {
+        string _title = """
+            _____________        _____________________     
+            ___  __ \__(_)__________  __/__(_)_____  /____ 
+            __  /_/ /_  /___  __ \_  /  __  /_  __  /_  _ \
+            _  _, _/_  / __  /_/ /  /   _  / / /_/ / /  __/
+            /_/ |_| /_/  _  .___//_/    /_/  \__,_/  \___/ 
+                         /_/                               
+            """;
+        string _options = """
+            1. Login
+            2. Register
+            3. Exit
+            """;
+    }
+}
